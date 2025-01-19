8d8d48837eef3557d52f1f48dafdacb42eec380e
Agnika Routh <agnika.routh@gmail.com>
2025-01-19 21:45:41 +0530

    handle routing error-handle route file and imports properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 // import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [AppRoutingModule],
+  imports: [CommonModule, RouterModule],
 })
 export class AppComponent {
   title = 'angular-boilerplate';
