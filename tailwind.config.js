792b3cfc4aeb8eec5cfe4390c3840c5ff39d61e3
Agnika Routh <agnika.routh@gmail.com>
2025-04-20 13:14:00 +0530

    added tailwind classes to the seach page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: "#f5f5dc",
+        cadetblue: "cadetblue",
+      },
+    },
   },
   plugins: [],
-}
+};
